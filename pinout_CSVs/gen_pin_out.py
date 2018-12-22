3916166a60ddf1fd46da3a8fcdffbbc41d6dde70
logickee <logickee@gmail.com>
2018-12-22 17:35:46 +0800

    update for RClamp0524PA,

--- pinout_CSVs/gen_pin_out.py
+++ pinout_CSVs/gen_pin_out.py
@@ -12,8 +12,29 @@ from pprint import pprint
 DRAW_Y_START_POS=750
 Y_SPACING=100
 
-draw_template="""DRAW\n{}\nENDDRAW"""
-pin_print_template='X {} {} -500 {} 200 R 50 50 1 1 I'
+CWD = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(CWD)
+sys.path.append(CWD+'/_lib')
+
+
+STAGING_COMPONENT_TEMPLATE="""EESchema-LIBRARY Version 2.4
+#encoding utf-8
+#
+# STAGING_COMPONENT
+#
+DEF STAGING_COMPONENT U 0 40 Y Y 1 F N
+F0 "U" 0 -1550 50 H V C CNN
+F1 "STAGING_COMPONENT" 0 -1700 50 H V C CNN
+F2 "" -500 100 50 H I C CNN
+F3 "" -500 100 50 H I C CNN
+{}
+#
+#End Library
+"""
+
+DEF_TEMPALTE="""{}\nENDDEF"""
+DRAW_TEMPLATE="""DRAW\n{}\nENDDRAW"""
+PIN_PRINT_TEMPLATE='X {} {} -500 {} 200 R 50 50 1 1 I'
 
 def readCSV(csv_file):
     with open(csv_file,'r') as f:
@@ -26,15 +47,18 @@ def readCSV(csv_file):
 
 def getPins(pin_confs):
     kicad_pins = [
-        pin_print_template.format(pin_confs[i],i+1, DRAW_Y_START_POS-(i*Y_SPACING)) for i in range(0,len(pin_confs))
+        PIN_PRINT_TEMPLATE.format(pin_confs[i],i+1, DRAW_Y_START_POS-(i*Y_SPACING)) for i in range(0,len(pin_confs))
     ]
     return kicad_pins
 
 def writePins(kicad_pins):
 
-    print('\n'.join(kicad_pins))
+    text_pins='\n'.join(kicad_pins)
+    text_draw = DRAW_TEMPLATE.format(text_pins)
+    text_def = DEF_TEMPALTE.format(text_draw)
+    print(STAGING_COMPONENT_TEMPLATE.format(text_def))
 
-pin_confs = readCSV('/home/logic/_workspace/kicad/lib_value_generator/pinout_CSVs/HDMI_19_PLUG_TYPEA.csv')
+pin_confs = readCSV('{}/RClamp0524PA.csv'.format(CWD))
 
 kicad_pins = getPins(pin_confs)
 writePins(kicad_pins)
